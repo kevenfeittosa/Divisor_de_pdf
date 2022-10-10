7b2f109c7e2c1559957d566bd853cf6ae2832cd1
Keven Feitosa <81097829+kevenfeittosa@users.noreply.github.com>
2022-10-10 14:28:26 -0300

    Update PDFSplitter.py

--- PDFSplitter.py
+++ PDFSplitter.py
@@ -4,47 +4,35 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 from pathlib import Path
 import shutil
 
-# informando o nome do aqruivo que ira ser usado
+pdf_file_path = () # Caminho do arquivo
 
-diretorio = os.listdir (r'C:\Users\25605\Desktop\demonstrativo')
-list_dir = r"C:/Users/25605/Desktop/demonstrativo/"
-print(diretorio)
-print(list_dir)
+# Retirando a extensão ".PDF" e substituindo por nada
+#file_base_name = pdf_file_path.replace('.pdf', '')
 
-for item in diretorio:
-    
+# Caminho de saída
+output_folder_path = os.path.join(os.getcwd(), 'Output')
 
-    # Caminho do diretorio + o arquivo informado
-    pdf_file_path = (str(list_dir)+str(item))
-    print(pdf_file_path)
+pdf = PdfFileReader(pdf_file_path)
 
-    # Retirando a extensão ".PDF" e substituindo por nada
-    #file_base_name = pdf_file_path.replace('.pdf', '')
+# Pecorrendo cada página do PDF
+for page_num in range(pdf.numPages):
+    pdfWriter = PdfFileWriter()
+    pdfWriter.addPage(pdf.getPage(page_num))
 
-    # Caminho de saída
-    output_folder_path = os.path.join(os.getcwd(), 'Output')
- 
-    pdf = PdfFileReader(pdf_file_path)
+    numero_da_pagina = page_num + 1
 
-    # Pecorrendo cada página do PDF
-    for page_num in range(pdf.numPages):
-        pdfWriter = PdfFileWriter()
-        pdfWriter.addPage(pdf.getPage(page_num))
+    # Criando um novo PDF para cada página - Com o mesmo nome do principal + o numero correspondente da página
+    with open(os.path.join(output_folder_path, f'{item}_pagina_{numero_da_pagina}.pdf'), 'wb') as f:
+        pdfWriter.write(f)
+        f.close()
+        # Verifica se o PDF foi gerado
+        check = 'OK'
+    # Caso tenha sido, ele vai mover esse PDF para outro diretório
+    if check == 'OK':
+        origem_download = Path(output_folder_path, f'{item}_pagina_{numero_da_pagina}.pdf')
+        destino = Path() # Caminho da pasta para onde o arquivo sera movido
+        shutil.move(origem_download, destino)
 
-        numero_da_pagina = page_num + 1
-
-        # Criando um novo PDF para cada página - Com o mesmo nome do principal + o numero correspondente da página
-        with open(os.path.join(output_folder_path, f'{item}_pagina_{numero_da_pagina}.pdf'), 'wb') as f:
-            pdfWriter.write(f)
-            f.close()
-            # Verifica se o PDF foi gerado
-            check = 'OK'
-        # Caso tenha sido, ele vai mover esse PDF para outro diretório
-        if check == 'OK':
-            origem_download = Path(output_folder_path, f'{item}_pagina_{numero_da_pagina}.pdf')
-            destino = Path(r"\\srvcid27\RPA_ARQUIVOS\CHAVE_FGTS\ORIGEM\\")
-            shutil.move(origem_download, destino)
-
-    print('#########################')
-    print('# 'f'{pdf.numPages} '+'Páginas divididas #')
-    print('#########################')
+print('#########################')
+print('# 'f'{pdf.numPages} '+'Páginas divididas #')
+print('#########################')
